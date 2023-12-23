f8eeec681140b1978c09cf6dbc24df9a867ec98d
mustafak <mustafa.kusku@sistek.com.tr>
2023-12-23 16:49:21 +0300

    let guncellendi

--- js/let-const-var-ile-degisken-tanimlamak.js
+++ js/let-const-var-ile-degisken-tanimlamak.js
@@ -3,3 +3,37 @@ console.log(user1)
 
 let user;
 console.log(user)
+
+user="system User"
+user="admin"
+console.log(user)
+
+const PASSWORD =1234;
+console.log("Sifre=",PASSWORD)
+
+{
+    let user="Gamze"
+    console.log("blok icerisinde",user)
+    const PASSWORD="3296238239"
+    console.count("Blokicinde",PASSWORD)
+}  
+console.log(user)
+console.log("Sifre=",PASSWORD) 
+console.log("24 uncu satira ulasabilirmiyiz",userName)
+
+var userName = "24.uncu satirdaki bilgi"
+console.info("userName=",userName)
+{
+    userName= "Blok icinde"
+    console.info("userName Blok",userName)
+    
+    
+    
+}
+
+var langu = "En"
+console.log("Dil",langu)
+
+isMacOsUser = true
+isPcUser= false
+hasUserPassword = true
