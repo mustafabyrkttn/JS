83cf1d7cd75cba43457caab8411a46af2c3e0cb5
mustafak <mustafa.kusku@sistek.com.tr>
2023-12-30 14:57:25 +0300

    guncel

---
+++ js/matematikselislemler.js
@@ -0,0 +1,21 @@
+let userId = 37
+console.log("Tek mi çift mi ?",userId % 2)
+
+userId ++
+console.log("Tek mi çift mi ?",userId % 2)
+
+
+const boxSize = 8
+let items = 29
+
+console.log("Son koliye kac urun var",items % boxSize)
+console.log("Son koliye kac adet urun eklenmeli",boxSize-(items % boxSize))
+
+console.log(3*3*3*3)
+console.log(3 ** 4)
+
+console.log("asagiya yuvarla",Math.floor(3.9))
+console.log("yukari yuvarlama",Math.ceil(3.9))
+console.log("yakina yuvarlama 3.9",Math.round(3.9))
+console.log("yakina yuvarlama 3.49",Math.round(3.49))
+console.log("yakina yuvarlama 3.51",Math.round(3.51))
