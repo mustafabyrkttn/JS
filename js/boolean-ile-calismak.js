58e8743e93adaa2eaf4eb3a0ca772dd7fc3a6c5d
mustafak <mustafa.kusku@sistek.com.tr>
2023-12-30 14:57:15 +0300

    boolean-ile

---
+++ js/boolean-ile-calismak.js
@@ -0,0 +1,14 @@
+let isUserActive = false
+
+isUserActive = true
+console.log(isUserActive)
+
+let user;
+console.log(user)
+console.log(Boolean(user))
+console.log("1-4",4>3)
+
+console.log(Boolean(-0),Boolean(0===0))
+
+user="LoremIpsum"
+console.log("Boolean(user)",Boolean(user))
