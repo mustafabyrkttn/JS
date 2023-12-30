5e721243acea407824d068fe15ae9a75f899fdf1
mustafak <mustafa.kusku@sistek.com.tr>
2023-12-30 18:54:40 +0300

    promot

---
+++ js/promot-alert-confirm.js
@@ -0,0 +1,15 @@
+//const info = prompt("adiniz","js")
+
+//alert(info)
+//console.log(info
+//console.log(
+  //  confirm("kullanici silinsin mi?"))
+
+
+//if  (confirm("Kullanici silinsin mi?")){
+//    console.log("kullanici silindi")
+//}
+
+
+
+
