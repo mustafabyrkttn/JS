1a8e3bf1e0cd1a2eb95309f0836caa9eb8089b0f
mustafak <mustafa.kusku@sistek.com.tr>
2023-12-30 14:57:34 +0300

    guncel

---
+++ js/number-veri.js
@@ -0,0 +1,37 @@
+let price = 100
+console.log("fiyat=",price)
+
+console.log(price*5)
+console.log(price)
+
+price=price+500
+console.log(price)
+
+price=90
+console.log(price)
+let count = 0
+console.log(count)
+count++
+console.log("count++",count)
+count++
+console.log("count++",count)
+count++
+console.log("count++",count)
+count--
+console.log("count--",count)
+count+=1000
+console.log("count+=1000",count)
+count-=10
+console.log("count-=10",count)
+count/=3
+console.log("count/=3",count)
+
+
+const totalPrice = price *1.18
+console.log("kdv",totalPrice)
+
+
+price+=1000
+price++
+console.log("fiyat=",price)
+
