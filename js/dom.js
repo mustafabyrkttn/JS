262d469ba134e04d8e87de9bb551e535cc0a8ede
mustafak <mustafa.kusku@sistek.com.tr>
2024-01-02 23:01:42 +0300

    dom.js eklendi

---
+++ js/dom.js
@@ -0,0 +1,23 @@
+
+console.log(
+    document.getElementsByClassName('text-primary')
+)
+
+
+const h1Element = document.querySelector('h1')
+const firstTitleCalss = document.querySelector('.title')
+const titleId = document.querySelector ('#title')
+
+
+
+const appElement = document.querySelector('#app')
+appElement.innerHTML = "<button>REACT APP ALANI</button>"
+
+console.log(firstTitleCalss)
+console.log(titleId)
+console.log(h1Element)
+console.log(h1Element.innerHTML)
+
+
+const listItems = document.querySelectorAll('.list-item')
+console.log(listItems)
